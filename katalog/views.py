06254c3372a1e6ea4168a7f72fea17b8ae37e63c
aidahputri <aidahptr15@gmail.com>
2022-09-09 19:24:05 +0700

    add everything

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+
+def show_catalog(request):
+    data_catalog_items = CatalogItem.objects.all()
+    context = {
+        'catalog_item': data_catalog_items,
+        'name': 'Aidah Novallia Putri',
+        'npm': '2106653400'
+    }
+    return render(request, "katalog.html", context)
